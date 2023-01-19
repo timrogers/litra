ee9f0a63f51454da15467b0427c7b2f691c4b503
Tim Rogers <me@timrogers.co.uk>
2023-01-19 10:34:08 +0000

    feat: expose the device type on each `Device`, and expose and enforce permitted brightness and temperature ranges for each device

--- tests/driver.test.ts
+++ tests/driver.test.ts
@@ -1,4 +1,9 @@
 import {
+  DeviceType,
+  getMaximumBrightnessInLumenForDevice,
+  getMinimumBrightnessInLumenForDevice,
+  getMaximumTemperatureInKelvinForDevice,
+  getMinimumTemperatureInKelvinForDevice,
   setBrightnessInLumen,
   setBrightnessPercentage,
   setTemperatureInKelvin,
@@ -9,11 +14,11 @@ import {
 
 describe('turnOn', () => {
   it('sends the instruction to turn the device on', () => {
-    const fakeDevice = { write: jest.fn() };
+    const fakeDevice = { type: DeviceType.LitraGlow, hid: { write: jest.fn() } };
 
     turnOn(fakeDevice);
 
-    expect(fakeDevice.write).toBeCalledWith([
+    expect(fakeDevice.hid.write).toBeCalledWith([
       17, 255, 4, 28, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     ]);
   });
@@ -21,11 +26,11 @@ describe('turnOn', () => {
 
 describe('turnOff', () => {
   it('sends the instruction to turn the device off', () => {
-    const fakeDevice = { write: jest.fn() };
+    const fakeDevice = { type: DeviceType.LitraGlow, hid: { write: jest.fn() } };
 
     turnOff(fakeDevice);
 
-    expect(fakeDevice.write).toBeCalledWith([
+    expect(fakeDevice.hid.write).toBeCalledWith([
       17, 255, 4, 28, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     ]);
   });
@@ -33,33 +38,45 @@ describe('turnOff', () => {
 
 describe('setTemperatureInKelvin', () => {
   it('sends the instruction to set the device temperature', () => {
-    const fakeDevice = { write: jest.fn() };
+    const fakeDevice = { type: DeviceType.LitraGlow, hid: { write: jest.fn() } };
 
     setTemperatureInKelvin(fakeDevice, 6300);
 
-    expect(fakeDevice.write).toBeCalledWith([
+    expect(fakeDevice.hid.write).toBeCalledWith([
       17, 255, 4, 156, 24, 156, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     ]);
   });
 
-  it('throws an error if the temperature is below the minimum', () => {
-    const fakeDevice = { write: jest.fn() };
+  it('throws an error if the temperature is below the minimum for the device', () => {
+    const fakeLitraGlow = { type: DeviceType.LitraGlow, hid: { write: jest.fn() } };
 
-    expect(() => setTemperatureInKelvin(fakeDevice, 2699)).toThrowError(
+    expect(() => setTemperatureInKelvin(fakeLitraGlow, 2699)).toThrowError(
+      'Provided temperature must be between 2700 and 6500',
+    );
+
+    const fakeLitraBeam = { type: DeviceType.LitraBeam, hid: { write: jest.fn() } };
+
+    expect(() => setTemperatureInKelvin(fakeLitraBeam, 2699)).toThrowError(
       'Provided temperature must be between 2700 and 6500',
     );
   });
 
-  it('throws an error if the temperature is above the maximum', () => {
-    const fakeDevice = { write: jest.fn() };
+  it('throws an error if the temperature is above the maximum for the device', () => {
+    const fakeLitraGlow = { type: DeviceType.LitraGlow, hid: { write: jest.fn() } };
 
-    expect(() => setTemperatureInKelvin(fakeDevice, 6501)).toThrowError(
+    expect(() => setTemperatureInKelvin(fakeLitraGlow, 6501)).toThrowError(
+      'Provided temperature must be between 2700 and 6500',
+    );
+
+    const fakeLitraBeam = { type: DeviceType.LitraBeam, hid: { write: jest.fn() } };
+
+    expect(() => setTemperatureInKelvin(fakeLitraBeam, 6501)).toThrowError(
       'Provided temperature must be between 2700 and 6500',
     );
   });
 
   it('throws an error if the temperature is not an integer', () => {
-    const fakeDevice = { write: jest.fn() };
+    const fakeDevice = { type: DeviceType.LitraGlow, hid: { write: jest.fn() } };
 
     expect(() => setTemperatureInKelvin(fakeDevice, 1337.9)).toThrowError(
       'Provided temperature must be an integer',
@@ -68,28 +85,44 @@ describe('setTemperatureInKelvin', () => {
 });
 
 describe('setTemperaturePercentage', () => {
-  it('sends the instruction to set the device temperature based on a percentage', () => {
-    const fakeDevice = { write: jest.fn() };
+  it('sends the instruction to set the device temperature based on a percentage, ', () => {
+    const fakeLitraGlow = { type: DeviceType.LitraGlow, hid: { write: jest.fn() } };
 
-    setTemperaturePercentage(fakeDevice, 100);
+    setTemperaturePercentage(fakeLitraGlow, 100);
 
-    expect(fakeDevice.write).toBeCalledWith([
+    expect(fakeLitraGlow.hid.write).toBeCalledWith([
+      17, 255, 4, 156, 25, 100, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    ]);
+
+    const fakeLitraBeam = { type: DeviceType.LitraBeam, hid: { write: jest.fn() } };
+
+    setTemperaturePercentage(fakeLitraBeam, 100);
+
+    expect(fakeLitraBeam.hid.write).toBeCalledWith([
       17, 255, 4, 156, 25, 100, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     ]);
   });
 
   it('sends the instruction to set the device temperature to the minimum temperature when set to 0%', () => {
-    const fakeDevice = { write: jest.fn() };
+    const fakeLitraGlow = { type: DeviceType.LitraGlow, hid: { write: jest.fn() } };
 
-    setTemperaturePercentage(fakeDevice, 0);
+    setTemperaturePercentage(fakeLitraGlow, 0);
 
-    expect(fakeDevice.write).toBeCalledWith([
+    expect(fakeLitraGlow.hid.write).toBeCalledWith([
+      17, 255, 4, 156, 10, 140, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    ]);
+
+    const fakeLitraBeam = { type: DeviceType.LitraBeam, hid: { write: jest.fn() } };
+
+    setTemperaturePercentage(fakeLitraBeam, 0);
+
+    expect(fakeLitraBeam.hid.write).toBeCalledWith([
       17, 255, 4, 156, 10, 140, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     ]);
   });
 
   it('throws an error if the provided percentage is less than 0', () => {
-    const fakeDevice = { write: jest.fn() };
+    const fakeDevice = { type: DeviceType.LitraGlow, hid: { write: jest.fn() } };
 
     expect(() => setTemperaturePercentage(fakeDevice, -1)).toThrowError(
       'Percentage must be between 0 and 100',
@@ -97,7 +130,7 @@ describe('setTemperaturePercentage', () => {
   });
 
   it('throws an error if the provided percentage is more than 100', () => {
-    const fakeDevice = { write: jest.fn() };
+    const fakeDevice = { type: DeviceType.LitraGlow, hid: { write: jest.fn() } };
 
     expect(() => setTemperaturePercentage(fakeDevice, 101)).toThrowError(
       'Percentage must be between 0 and 100',
@@ -107,33 +140,45 @@ describe('setTemperaturePercentage', () => {
 
 describe('setBrightnessInLumen', () => {
   it('sends the instruction to set the device temperature', () => {
-    const fakeDevice = { write: jest.fn() };
+    const fakeDevice = { type: DeviceType.LitraGlow, hid: { write: jest.fn() } };
 
     setBrightnessInLumen(fakeDevice, 20);
 
-    expect(fakeDevice.write).toBeCalledWith([
+    expect(fakeDevice.hid.write).toBeCalledWith([
       17, 255, 4, 76, 0, 20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     ]);
   });
 
-  it('throws an error if the brightness is below the minimum', () => {
-    const fakeDevice = { write: jest.fn() };
+  it('throws an error if the brightness is below the minimum for the device', () => {
+    const fakeLitraGlow = { type: DeviceType.LitraGlow, hid: { write: jest.fn() } };
 
-    expect(() => setBrightnessInLumen(fakeDevice, 19)).toThrowError(
+    expect(() => setBrightnessInLumen(fakeLitraGlow, 19)).toThrowError(
       'Provided brightness must be between 20 and 250',
+    );
+
+    const fakeLitraBeam = { type: DeviceType.LitraBeam, hid: { write: jest.fn() } };
+
+    expect(() => setBrightnessInLumen(fakeLitraBeam, 19)).toThrowError(
+      'Provided brightness must be between 20 and 400',
     );
   });
 
-  it('throws an error if the brightness is above the maximum', () => {
-    const fakeDevice = { write: jest.fn() };
+  it('throws an error if the brightness is above the maximum for the device', () => {
+    const fakeLitraGlow = { type: DeviceType.LitraGlow, hid: { write: jest.fn() } };
 
-    expect(() => setBrightnessInLumen(fakeDevice, 251)).toThrowError(
+    expect(() => setBrightnessInLumen(fakeLitraGlow, 251)).toThrowError(
       'Provided brightness must be between 20 and 250',
+    );
+
+    const fakeLitraBeam = { type: DeviceType.LitraBeam, hid: { write: jest.fn() } };
+
+    expect(() => setBrightnessInLumen(fakeLitraBeam, 401)).toThrowError(
+      'Provided brightness must be between 20 and 400',
     );
   });
 
   it('throws an error if the brightness is not an integer', () => {
-    const fakeDevice = { write: jest.fn() };
+    const fakeDevice = { type: DeviceType.LitraGlow, hid: { write: jest.fn() } };
 
     expect(() => setBrightnessInLumen(fakeDevice, 1337.9)).toThrowError(
       'Provided brightness must be an integer',
@@ -143,27 +188,43 @@ describe('setBrightnessInLumen', () => {
 
 describe('setBrightnessPercentage', () => {
   it('sends the instruction to set the device brightness based on a percentage', () => {
-    const fakeDevice = { write: jest.fn() };
+    const fakeLitraGlow = { type: DeviceType.LitraGlow, hid: { write: jest.fn() } };
 
-    setBrightnessPercentage(fakeDevice, 100);
+    setBrightnessPercentage(fakeLitraGlow, 100);
 
-    expect(fakeDevice.write).toBeCalledWith([
+    expect(fakeLitraGlow.hid.write).toBeCalledWith([
       17, 255, 4, 76, 0, 250, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    ]);
+
+    const fakeLitraBeam = { type: DeviceType.LitraBeam, hid: { write: jest.fn() } };
+
+    setBrightnessPercentage(fakeLitraBeam, 100);
+
+    expect(fakeLitraBeam.hid.write).toBeCalledWith([
+      17, 255, 4, 76, 0, 400, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     ]);
   });
 
   it('sends the instruction to set the device brightness to the minimum brightness when set to 0%', () => {
-    const fakeDevice = { write: jest.fn() };
+    const fakeLitraGlow = { type: DeviceType.LitraGlow, hid: { write: jest.fn() } };
 
-    setBrightnessPercentage(fakeDevice, 0);
+    setBrightnessPercentage(fakeLitraGlow, 0);
 
-    expect(fakeDevice.write).toBeCalledWith([
+    expect(fakeLitraGlow.hid.write).toBeCalledWith([
+      17, 255, 4, 76, 0, 20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    ]);
+
+    const fakeLitraBeam = { type: DeviceType.LitraBeam, hid: { write: jest.fn() } };
+
+    setBrightnessPercentage(fakeLitraBeam, 0);
+
+    expect(fakeLitraBeam.hid.write).toBeCalledWith([
       17, 255, 4, 76, 0, 20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     ]);
   });
 
   it('throws an error if the provided percentage is less than 0', () => {
-    const fakeDevice = { write: jest.fn() };
+    const fakeDevice = { type: DeviceType.LitraGlow, hid: { write: jest.fn() } };
 
     expect(() => setBrightnessPercentage(fakeDevice, -1)).toThrowError(
       'Percentage must be between 0 and 100',
@@ -171,10 +232,58 @@ describe('setBrightnessPercentage', () => {
   });
 
   it('throws an error if the provided percentage is more than 100', () => {
-    const fakeDevice = { write: jest.fn() };
+    const fakeDevice = { type: DeviceType.LitraGlow, hid: { write: jest.fn() } };
 
     expect(() => setBrightnessPercentage(fakeDevice, 101)).toThrowError(
       'Percentage must be between 0 and 100',
     );
+  });
+});
+
+describe('getMinimumBrightnessInLumenForDevice', () => {
+  it('returns the correct minimum brightness for a Litra Glow', () => {
+    const fakeDevice = { type: DeviceType.LitraGlow, hid: { write: jest.fn() } };
+    expect(getMinimumBrightnessInLumenForDevice(fakeDevice)).toEqual(20);
+  });
+
+  it('returns the correct minimum brightness for a Litra Beam', () => {
+    const fakeDevice = { type: DeviceType.LitraBeam, hid: { write: jest.fn() } };
+    expect(getMinimumBrightnessInLumenForDevice(fakeDevice)).toEqual(20);
+  });
+});
+
+describe('getMaximumBrightnessInLumenForDevice', () => {
+  it('returns the correct maximum brightness for a Litra Glow', () => {
+    const fakeDevice = { type: DeviceType.LitraGlow, hid: { write: jest.fn() } };
+    expect(getMaximumBrightnessInLumenForDevice(fakeDevice)).toEqual(250);
+  });
+
+  it('returns the correct maximum brightness for a Litra Beam', () => {
+    const fakeDevice = { type: DeviceType.LitraBeam, hid: { write: jest.fn() } };
+    expect(getMaximumBrightnessInLumenForDevice(fakeDevice)).toEqual(400);
+  });
+});
+
+describe('getMinimumTemperatureInKelvinForDevice', () => {
+  it('returns the correct minimum temperature for a Litra Glow', () => {
+    const fakeDevice = { type: DeviceType.LitraGlow, hid: { write: jest.fn() } };
+    expect(getMinimumTemperatureInKelvinForDevice(fakeDevice)).toEqual(2700);
+  });
+
+  it('returns the correct minimum temperature for a Litra Beam', () => {
+    const fakeDevice = { type: DeviceType.LitraBeam, hid: { write: jest.fn() } };
+    expect(getMinimumTemperatureInKelvinForDevice(fakeDevice)).toEqual(2700);
+  });
+});
+
+describe('getMaximumTemperatureInKelvinForDevice', () => {
+  it('returns the correct maximum temperature for a Litra Glow', () => {
+    const fakeDevice = { type: DeviceType.LitraGlow, hid: { write: jest.fn() } };
+    expect(getMaximumTemperatureInKelvinForDevice(fakeDevice)).toEqual(6500);
+  });
+
+  it('returns the correct maximum temperature for a Litra Beam', () => {
+    const fakeDevice = { type: DeviceType.LitraBeam, hid: { write: jest.fn() } };
+    expect(getMaximumTemperatureInKelvinForDevice(fakeDevice)).toEqual(6500);
   });
 });
