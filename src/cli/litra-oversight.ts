e2efb2a59ae5c6f14c995b22a3add4728594abb9
Tim Rogers <me@timrogers.co.uk>
2023-02-10 13:32:43 +0100

    feat: improve Oversight integration with `litra-oversight` to target all available devices
    This commit also reworks `litra-oversight`'s command line option
    parsing to use Commander, and adds a `--help` argument with docs.

--- src/cli/litra-oversight.ts
+++ src/cli/litra-oversight.ts
@@ -1,32 +1,49 @@
 #!/usr/bin/env node
 
-import { findDevice, turnOff, turnOn } from './../driver';
+import { program } from 'commander';
+import { findDevices, turnOff, turnOn } from './../driver';
+
+program
+  .name('litra-oversight')
+  .description(
+    'Connects Oversight (<https://objective-see.org/products/oversight.html>) with Litra devices, allowing you to turn your Litra device on and off automatically with your webcam.',
+  )
+  .option(
+    '-device <device>',
+    'the type of device that was turned on or off, either `camera` or `microphone`',
+  )
+  .option('-event <event>', 'the event that happened to the device, either `on` or `off`')
+  .option(
+    '-process <process>',
+    'the PID of the process that triggered the event (ignored)',
+  );
+
+program.parse();
+// Commander isn't really designed to support these single dash arguments. When arguments are specified this way,
+// the key is starts with a capital letter, which isn't the normal behaviour.
+const { Device: deviceType, Event: event } = program.opts();
 
 try {
-  const device = findDevice();
+  const devices = findDevices();
 
-  if (!device) {
-    throw 'Device not found';
-  }
+  if (devices.length) {
+    const isCameraArg = deviceType === 'camera';
+    const isEventOnArg = event === 'on';
 
-  const DEVICE_ARG_VAL = 'camera';
-  const EVENT_ARG_VAL = 'on';
-
-  const deviceArgVal = process.argv[3];
-  const eventArgVal = process.argv[5];
-
-  const isCameraArg = deviceArgVal == DEVICE_ARG_VAL;
-  const isEventOnArg = eventArgVal == EVENT_ARG_VAL;
-
-  if (isCameraArg) {
-    if (isEventOnArg) {
-      turnOn(device);
-    } else {
-      turnOff(device);
+    for (const device of devices) {
+      if (isCameraArg) {
+        if (isEventOnArg) {
+          turnOn(device);
+        } else {
+          turnOff(device);
+        }
+      }
     }
-  }
 
-  process.exit(0);
+    process.exit(0);
+  } else {
+    throw 'No devices found';
+  }
 } catch (e) {
   console.log(e);
   process.exit(1);
