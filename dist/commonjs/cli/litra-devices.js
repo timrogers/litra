439bd5147f6e5fff8fb3ba0c85aa08721191e1c9
Tim Rogers <me@timrogers.co.uk>
2023-02-11 19:06:45 +0000

    chore: build Typescript

---
+++ dist/commonjs/cli/litra-devices.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const commander_1 = require("commander");
+const driver_1 = require("../driver");
+commander_1.program
+    .name('litra-devices')
+    .description('Lists Litra devices connected to your computer')
+    .option('--json', 'output the list of devices in structured JSON format');
+commander_1.program.parse();
+const { json } = commander_1.program.opts();
+const devices = (0, driver_1.findDevices)();
+if (json) {
+    console.log(JSON.stringify(devices.map((device) => ({
+        name: (0, driver_1.getNameForDevice)(device),
+        serial_number: device.serialNumber,
+    }))));
+}
+else {
+    if (devices.length) {
+        for (const device of devices) {
+            console.log(`- ${(0, driver_1.getNameForDevice)(device)} (${device.serialNumber})`);
+        }
+    }
+    else {
+        console.log('No devices found');
+    }
+}
+process.exit(0);
---
+++ dist/esm/cli/litra-devices.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+import { program } from 'commander';
+import { findDevices, getNameForDevice } from '../driver';
+program
+    .name('litra-devices')
+    .description('Lists Litra devices connected to your computer')
+    .option('--json', 'output the list of devices in structured JSON format');
+program.parse();
+const { json } = program.opts();
+const devices = findDevices();
+if (json) {
+    console.log(JSON.stringify(devices.map((device) => ({
+        name: getNameForDevice(device),
+        serial_number: device.serialNumber,
+    }))));
+}
+else {
+    if (devices.length) {
+        for (const device of devices) {
+            console.log(`- ${getNameForDevice(device)} (${device.serialNumber})`);
+        }
+    }
+    else {
+        console.log('No devices found');
+    }
+}
+process.exit(0);
