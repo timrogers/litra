ef36bdcb7d09c3af2d9cd849bae8a5f475c362b4
Houssam Haidar <haidar647@gmail.com>
2023-03-28 11:04:40 -0400

    Update command descriptions

--- dist/esm/cli/litra-temperature-k.js
+++ dist/esm/cli/litra-temperature-k.js
@@ -4,7 +4,7 @@ import { getTemperatureInKelvin, setTemperatureInKelvin } from '../driver';
 import { getDeviceForCLI, parseIntOption } from './utils';
 program
     .name('litra-temperature-k')
-    .description('Sets the temperature of a Litra device to a value in Kelvin')
+    .description("Manages the temperature of a Litra device. If a temperature argument is provided, it sets the device's temperature to the supplied value in Kelvin. If no temperature argument is provided, it returns the current temperature in Kelvin.")
     .option('-s, --serial-number <serialNumber>', 'serial number of the Litra device. If this is not specified and multiple devices are connected, this will default to the first identified device, which is not guaranteed to be the same every time you run this command')
     .argument('[temperature]', 'the temperature in Kelvin, e.g. `2000`', parseIntOption);
 program.parse();
