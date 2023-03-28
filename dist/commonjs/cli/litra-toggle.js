80a53caf26cd6b509c916ae48df4033b2baa2afe
Tim Rogers <timrogers@github.com>
2023-03-28 18:46:55 +0100

    chore: build Typescript

--- dist/commonjs/cli/litra-toggle.js
+++ dist/commonjs/cli/litra-toggle.js
@@ -6,7 +6,7 @@ const driver_1 = require("../driver");
 const utils_1 = require("./utils");
 commander_1.program
     .name('litra-toggle')
-    .description('Toggle a Litra device on or off')
+    .description('Toggles a Litra device on or off')
     .option('-s, --serial-number <serialNumber>', 'serial number of the Litra device. If this is not specified and multiple devices are connected, this will default to the first identified device, which is not guaranteed to be the same every time you run this command');
 commander_1.program.parse();
 const { serialNumber } = commander_1.program.opts();
--- dist/esm/cli/litra-toggle.js
+++ dist/esm/cli/litra-toggle.js
@@ -4,7 +4,7 @@ import { toggle } from '../driver';
 import { getDeviceForCLI } from './utils';
 program
     .name('litra-toggle')
-    .description('Toggle a Litra device on or off')
+    .description('Toggles a Litra device on or off')
     .option('-s, --serial-number <serialNumber>', 'serial number of the Litra device. If this is not specified and multiple devices are connected, this will default to the first identified device, which is not guaranteed to be the same every time you run this command');
 program.parse();
 const { serialNumber } = program.opts();
